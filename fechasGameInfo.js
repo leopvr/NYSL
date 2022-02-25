ede1ff6e24f2074bb485dea1712300392e74b8e5
Leonardo Peñalver <leo.pvr13@gmail.com>
2022-02-25 16:35:41 -0300

    Landscape

---
+++ fechasGameInfo.js
@@ -0,0 +1,181 @@
+var data = [{
+    "month_name": "SEPTEMBER",
+    "games": [
+        {
+            "vsName":"U1 and U4",
+            "location":{
+                "name": "AJ Katzenmaier",
+                "Iframe": "https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=es&amp;q=1829%20Kennedy%20Dr,%20North%20Chicago,%20IL%2060088,%20Estados%20Unidos+(AJ%20Katzenmaier%20Academy%09)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps?ll=42.325868,-87.862801&z=14&t=m&hl=es&gl=US&mapclient=embed&q=1829+Kennedy+Dr+North+Chicago,+IL+60064+EE.+UU.",
+            },
+            "times":"9:30 a.m.",
+            "day":"01"
+        },
+        {
+            "vsName":"U3 and U2",
+            "location": {
+                "name" : "Greenbay",
+                "Iframe":"https://maps.google.com/maps?width=300&amp;height=300&amp;hl=es&amp;q=820%20Armed%20Forces%20Dr,%20Green%20Bay,%20WI%2054304+(Green%20Bay%20Gamblers%20Hockey)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/820+Armed+Forces+Dr,+Green+Bay,+WI+54304/@44.499394,-88.055104,14z/data=!4m5!3m4!1s0x8802fa546caa53a7:0x8e2b8e336a5f95f8!8m2!3d44.4993938!4d-88.0551042?hl=es",
+            },
+            "times":"1:00 p.m.",
+            "day":"01"
+        },
+        {
+            "vsName":"U5 and U6",
+            "location":{
+                "name": "Howard A Yeager",
+                "Iframe": "https://maps.google.com/maps?width=300&amp;height=300&amp;hl=es&amp;q=1811%20Morrow%20Ave,%20North%20Chicago,%20IL%2060064,%20Estados%20Unidos+(Howard%20A.%20Yeager%20School)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/1811+Morrow+Ave,+North+Chicago,+IL+60064/@42.315318,-87.853824,14z/data=!4m5!3m4!1s0x880feccaa32b2b93:0xc2120a8237aaa4e4!8m2!3d42.3153184!4d-87.8538245?hl=es"
+            },
+            "times":"9:30 a.m.",
+            "day":"08"
+        },
+        {
+            "vsName":"U6 and U1",
+            "location":{
+                "name": "Marjorie P Hart",
+                "Iframe": "https://maps.google.com/maps?width=400&amp;height=300&amp;hl=es&amp;q=N%20Orchard%20St,%20Chicago,%20IL%2060614,%20Estados%20Unidos+(Marjorie%20P%20Hart%20Elementary)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/N+Orchard+St,+Chicago,+IL+60614/@41.923523,-87.646262,14z/data=!4m5!3m4!1s0x880fd3102866e98b:0x6f95a4b19f6f6a1!8m2!3d41.9235227!4d-87.646262?hl=es",
+            },
+            "times":"1:00 p.m.",
+            "day":"08"
+        },
+        {
+            "vsName":"U2 and U4",
+            "location":{
+                "name": "North",
+                "Iframe": "https://maps.google.com/maps?width=400&amp;height=300&amp;hl=es&amp;q=2601%20Avenue%20of%20the%20Stars,%20Frisco,%20TX%2075034,%20Estados%20Unidos+(North%20American%20Hockey%20League)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/2601+Avenue+of+the+Stars,+Frisco,+TX+75034/@33.100657,-96.81986,14z/data=!4m5!3m4!1s0x864c3c96acb30463:0x8b43bf85a687e29e!8m2!3d33.1006569!4d-96.8198597?hl=es",
+            },
+            "times":"9:30 a.m.",
+            "day":"15"
+        },
+        {
+            "vsName":"U3 and U5",
+            "location":{
+                "name": "AJ Katzenmaier",
+                "Iframe": "https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=es&amp;q=1829%20Kennedy%20Dr,%20North%20Chicago,%20IL%2060088,%20Estados%20Unidos+(AJ%20Katzenmaier%20Academy%09)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps?ll=42.325868,-87.862801&z=14&t=m&hl=es&gl=US&mapclient=embed&q=1829+Kennedy+Dr+North+Chicago,+IL+60064+EE.+UU.",
+            },
+            "times":"1:00 p.m.",
+            "day":"15"
+        },
+        {
+            "vsName":"U1 and U3",
+            "location":{
+                "name": "South",
+                "Iframe": "https://maps.google.com/maps?width=400&amp;height=300&amp;hl=es&amp;q=1176%20Rufus%20Allen%20Blvd,%20South%20Lake%20Tahoe,%20CA%2096150,%20Estados%20Unidos+(South%20Lake%20Tahoe%20Ice%20Arena)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/1176+Rufus+Allen+Blvd,+South+Lake+Tahoe,+CA+96150/@38.941506,-119.974754,14z/data=!4m5!3m4!1s0x80998ffd5d34711f:0x3a6a3f32cc55ed15!8m2!3d38.9415061!4d-119.9747535?hl=es",
+            },
+            "times":"9:30 a.m.",
+            "day":"22"
+        },
+        {
+            "vsName":"U2 and U6",
+            "location":{
+                "name": "Howard A Yeager",
+                "Iframe": "https://maps.google.com/maps?width=300&amp;height=300&amp;hl=es&amp;q=1811%20Morrow%20Ave,%20North%20Chicago,%20IL%2060064,%20Estados%20Unidos+(Howard%20A.%20Yeager%20School)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/1811+Morrow+Ave,+North+Chicago,+IL+60064/@42.315318,-87.853824,14z/data=!4m5!3m4!1s0x880feccaa32b2b93:0xc2120a8237aaa4e4!8m2!3d42.3153184!4d-87.8538245?hl=es"
+            },
+            "times":"1:00 p.m.",
+            "day":"22"
+        },
+        {
+            "vsName":"U4 and U5",
+            "location": {
+                "name" : "Greenbay",
+                "Iframe":"https://maps.google.com/maps?width=300&amp;height=300&amp;hl=es&amp;q=820%20Armed%20Forces%20Dr,%20Green%20Bay,%20WI%2054304+(Green%20Bay%20Gamblers%20Hockey)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/820+Armed+Forces+Dr,+Green+Bay,+WI+54304/@44.499394,-88.055104,14z/data=!4m5!3m4!1s0x8802fa546caa53a7:0x8e2b8e336a5f95f8!8m2!3d44.4993938!4d-88.0551042?hl=es",
+            },
+            "times":"9:30 a.m.",
+            "day":"29"
+        }
+    ]
+},
+{
+    "month_name": "OCTOBER",
+    "games": [
+        {
+            "vsName":"U2 and U5",
+            "location":{
+                "name": "Marjorie P Hart",
+                "Iframe": "https://maps.google.com/maps?width=400&amp;height=300&amp;hl=es&amp;q=N%20Orchard%20St,%20Chicago,%20IL%2060614,%20Estados%20Unidos+(Marjorie%20P%20Hart%20Elementary)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/N+Orchard+St,+Chicago,+IL+60614/@41.923523,-87.646262,14z/data=!4m5!3m4!1s0x880fd3102866e98b:0x6f95a4b19f6f6a1!8m2!3d41.9235227!4d-87.646262?hl=es",
+            },
+            "times":"9:30 a.m.",
+            "day":"06"
+        },
+        {
+            "vsName":"U1 and U6",
+            "location":{
+                "name": "South",
+                "Iframe": "https://maps.google.com/maps?width=400&amp;height=300&amp;hl=es&amp;q=1176%20Rufus%20Allen%20Blvd,%20South%20Lake%20Tahoe,%20CA%2096150,%20Estados%20Unidos+(South%20Lake%20Tahoe%20Ice%20Arena)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/1176+Rufus+Allen+Blvd,+South+Lake+Tahoe,+CA+96150/@38.941506,-119.974754,14z/data=!4m5!3m4!1s0x80998ffd5d34711f:0x3a6a3f32cc55ed15!8m2!3d38.9415061!4d-119.9747535?hl=es",
+            },
+            "times":"1:00 p.m.",
+            "day":"06"
+        },
+        {
+            "vsName":"U3 and U4",
+            "location":{
+                "name": "Howard A Yeager",
+                "Iframe": "https://maps.google.com/maps?width=300&amp;height=300&amp;hl=es&amp;q=1811%20Morrow%20Ave,%20North%20Chicago,%20IL%2060064,%20Estados%20Unidos+(Howard%20A.%20Yeager%20School)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/1811+Morrow+Ave,+North+Chicago,+IL+60064/@42.315318,-87.853824,14z/data=!4m5!3m4!1s0x880feccaa32b2b93:0xc2120a8237aaa4e4!8m2!3d42.3153184!4d-87.8538245?hl=es"
+            },
+            "times":"9:30 a.m.",
+            "day":"08"
+        },
+        {
+            "vsName":"U5 and U1",
+            "location": {
+                "name" : "Greenbay",
+                "Iframe":"https://maps.google.com/maps?width=300&amp;height=300&amp;hl=es&amp;q=820%20Armed%20Forces%20Dr,%20Green%20Bay,%20WI%2054304+(Green%20Bay%20Gamblers%20Hockey)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/820+Armed+Forces+Dr,+Green+Bay,+WI+54304/@44.499394,-88.055104,14z/data=!4m5!3m4!1s0x8802fa546caa53a7:0x8e2b8e336a5f95f8!8m2!3d44.4993938!4d-88.0551042?hl=es",
+            },
+            "times":"1:00 p.m.",
+            "day":"08"
+        },
+        {
+            "vsName":"U6 and U3",
+            "location":{
+                "name": "North",
+                "Iframe": "https://maps.google.com/maps?width=400&amp;height=300&amp;hl=es&amp;q=2601%20Avenue%20of%20the%20Stars,%20Frisco,%20TX%2075034,%20Estados%20Unidos+(North%20American%20Hockey%20League)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/2601+Avenue+of+the+Stars,+Frisco,+TX+75034/@33.100657,-96.81986,14z/data=!4m5!3m4!1s0x864c3c96acb30463:0x8b43bf85a687e29e!8m2!3d33.1006569!4d-96.8198597?hl=es",
+            },
+            "times":"9:30 a.m.",
+            "day":"20	"
+        },
+        {
+            "vsName":"U2 and U4",
+            "location":{
+                "name": "Marjorie P Hart",
+                "Iframe": "https://maps.google.com/maps?width=400&amp;height=300&amp;hl=es&amp;q=N%20Orchard%20St,%20Chicago,%20IL%2060614,%20Estados%20Unidos+(Marjorie%20P%20Hart%20Elementary)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/N+Orchard+St,+Chicago,+IL+60614/@41.923523,-87.646262,14z/data=!4m5!3m4!1s0x880fd3102866e98b:0x6f95a4b19f6f6a1!8m2!3d41.9235227!4d-87.646262?hl=es",
+            },
+            "times":"1:00 p.m.",
+            "day":"20	"
+        },
+        {
+            "vsName":"U3 and U1",
+            "location":{
+                "name": "AJ Katzenmaier",
+                "Iframe": "https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=es&amp;q=1829%20Kennedy%20Dr,%20North%20Chicago,%20IL%2060088,%20Estados%20Unidos+(AJ%20Katzenmaier%20Academy%09)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps?ll=42.325868,-87.862801&z=14&t=m&hl=es&gl=US&mapclient=embed&q=1829+Kennedy+Dr+North+Chicago,+IL+60064+EE.+UU.",
+            },
+            "times":"9:30 a.m.",
+            "day":"27"
+        },
+        {
+            "vsName":"U5 and U6",
+            "location":{
+                "name": "Howard A Yeager",
+                "Iframe": "https://maps.google.com/maps?width=300&amp;height=300&amp;hl=es&amp;q=1811%20Morrow%20Ave,%20North%20Chicago,%20IL%2060064,%20Estados%20Unidos+(Howard%20A.%20Yeager%20School)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+                "linkGM": "https://www.google.com/maps/place/1811+Morrow+Ave,+North+Chicago,+IL+60064/@42.315318,-87.853824,14z/data=!4m5!3m4!1s0x880feccaa32b2b93:0xc2120a8237aaa4e4!8m2!3d42.3153184!4d-87.8538245?hl=es"
+            },
+            "times":"1:00 p.m.",
+            "day":"27"
+        }
+    ]
+},
+]
--- script.js
+++ script.js
@@ -1,5 +1,164 @@
+var screenVW = window.matchMedia("(orientation: portrait)")
+// var screenVH = window.matchMedia("(orientation: landscape)")
+
+window.addEventListener('resize', horScreen);
+
+function horScreen(p1) {
+
+    if (document.getElementById('tbody1') != undefined) {
+
+        if (p1.matches) {
+            // console.log(p1.matches);
+
+            data[0].games.forEach(element => {
+                let tr = document.createElement("tr");
+                tr.innerHTML =
+                    `<td style="width: 80px;">${element.day}</td>
+        <td>${element.vsName} <br> <a href="${element.location.linkGM}">${element.location.name}</a></td>
+        <td style="width: 80px;">${element.times}</td>`;
+                document.getElementById("tbody1").appendChild(tr);
+            });
+
+
+            data[1].games.forEach(element => {
+                let tr = document.createElement("tr");
+                tr.innerHTML =
+                    `<td style="width: 80px;">${element.day}</td>
+        <td>${element.vsName} <br> <a href="${element.location.linkGM}">${element.location.name}</a></td>
+        <td style="width: 80px;">${element.times}</td>`;
+                document.getElementById("tbody2").appendChild(tr);
+            });
+
+        } else {
+            // console.log(p1.matches);
+
+            document.getElementById("ulH").innerHTML = ' ';
+            document.getElementById("ulH2").innerHTML = ' ';
+
+
+            data[0].games.forEach(function (element) {
+                // console.log(index)
+                let ilH = document.createElement("button");
+                ilH.classList.add('bar');
+
+                ilH.innerHTML = `${element.vsName}`
+
+                document.getElementById("ulH").appendChild(ilH);
+                
+            })
+
+            document.getElementById('ulH').addEventListener("click", function(e){
+                const seleccionado = [...this.children]
+                .filter(el => el.className.indexOf('bar') > -1)
+                .indexOf(e.target);
+         
+
+              if (seleccionado > -1) {
+                seleccionado.style.backgroundColor = "red";
+
+                console.log(`position index: ${seleccionado}`);
+
+                document.getElementById('infoH').innerHTML = ' ';
+
+                function createInfo(s) {
+                    let ulH = document.createElement("div");              
+                    ulH.innerHTML = `<div class="infoT">
+                    <div class="d-flex flex-column justify-content-center">
+                        <p>Day:</p>
+                        <p style="color: orange;">${data[0].games[s].day}</p>
+                    </div>
+                    <div>
+                        <p>Times:</p>
+                        <p style="color: orange;">${data[0].games[s].times}</p>
+                    </div>
+                    <div>
+                        <p>Location:</p>
+                        <p style="color: orange;">${data[0].games[s].location.name}</p>
+                    </div>
+                </div>
+    
+                <div class="d-flex justify-content-center">
+                    <iframe class="Imap" style="box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;"
+                        src="${data[0].games[s].location.Iframe}">
+                    </iframe>
+                </div>`
+                    document.getElementById('infoH').appendChild(ulH);
+                    ulH.classList.add('acomodo');
+                }
+                createInfo(seleccionado);
+
+              } else {
+
+                  console.log("Nada");
+
+              }
+            });
+
+            data[1].games.forEach(function (element) {
+                // console.log(index)
+                let ilH = document.createElement("button");
+                ilH.classList.add('bar');
+
+                ilH.innerHTML = `${element.vsName}`
+
+                document.getElementById("ulH2").appendChild(ilH);
+                
+            })
+
+            document.getElementById('ulH2').addEventListener("click", function(e){
+                const seleccionado = [...this.children]
+                .filter(el => el.className.indexOf('bar') > -1)
+                .indexOf(e.target);
+         
+
+              if (seleccionado > -1) {
+
+                console.log(`position index: ${seleccionado}`);
+
+                document.getElementById('infoH2').innerHTML = ' ';
+
+                function createInfo(s) {
+                    let ulH = document.createElement("div");              
+                    ulH.innerHTML = `<div class="infoT">
+                    <div class="d-flex flex-column justify-content-center">
+                        <p>Day:</p>
+                        <p style="color: orange;">${data[1].games[s].day}</p>
+                    </div>
+                    <div>
+                        <p>Times:</p>
+                        <p style="color: orange;">${data[1].games[s].times}</p>
+                    </div>
+                    <div>
+                        <p>Location:</p>
+                        <p style="color: orange;">${data[1].games[s].location.name}</p>
+                    </div>
+                </div>
+    
+                <div class="d-flex justify-content-center">
+                    <iframe class="Imap" style="box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;"
+                        src="${data[1].games[s].location.Iframe}">
+                    </iframe>
+                </div>`
+                    document.getElementById('infoH2').appendChild(ulH);
+                    ulH.classList.add('acomodo');
+                }
+                createInfo(seleccionado);
+
+              } else {
+
+                  console.log("Nada");
+
+              }
+            });
+
+        }
+    }
+}
+horScreen(screenVW)
+
+
 const iconMenu = document.querySelector('#iconmenu'),
-menu = document.querySelector('#menu');
+    menu = document.querySelector('#menu');
 
 iconMenu.addEventListener('click', (e) => {
     menu.classList.toggle('active');
@@ -26,19 +185,19 @@ bdark.addEventListener('click', e => {
     store(body.classList.contains('darkmode'));
 })
 
-function load(){
+function load() {
     const dm = localStorage.getItem('darkmode');
 
     if (!dm) {
         store('false');
-    }else if (dm == 'true') {
+    } else if (dm == 'true') {
         body.classList.add('darkmode');
     }
-} 
+}
 
-function store(value){
+function store(value) {
     localStorage.setItem('darkmode', value)
-} 
+}
 
 
 
@@ -51,12 +210,6 @@ function moreInfo2() {
     var plus = document.querySelector('.moreInfo2');
     // var plustxt = document.getElementById("plustxt")
     plus.classList.toggle("moreInfoshow")
-
-//     if (plus.style.display === "none") {
-//         plustxt.innerHTML = "+";
-//     } else if (plus.style.display === "block"){
-//         plustxt.innerHTML = "-";
-//     }
 }
 
 function moreInfo3() {
@@ -64,12 +217,12 @@ function moreInfo3() {
     plus.classList.toggle("moreInfoshow")
 }
 
-function gameInfo1() {
-    var plus = document.querySelector('.gameInfo1');
-    plus.classList.toggle("lessInfoshow")
-}
+// function gameInfo1() {
+//     var plus = document.querySelector('.gameInfo1');
+//     plus.classList.toggle("lessInfoshow")
+// }
 
-function gameInfo2() {
-    var plus = document.querySelector('.gameInfo2');
-    plus.classList.toggle("moreInfoshow")
-}
+// function gameInfo2() {
+//     var plus = document.querySelector('.gameInfo2');
+//     plus.classList.toggle("moreInfoshow")
+// }
